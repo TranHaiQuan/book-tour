19545da32f6b5697f8ab86e10c14b25e0d6c5a2f
tran hai quan <tranhaiquan.thq@gmail.com>
2018-01-22 13:52:13 +0700

    ckeditor

--- config/application.rb
+++ config/application.rb
@@ -11,5 +11,8 @@ module Mytour1
     config.i18n.default_locale = :en
     config.action_view.embed_authenticity_token_in_remote_forms = true
     config.assets.initialize_on_precompile = false
+    config.assets.precompile += Ckeditor.assets
+    config.assets.precompile += %w( ckeditor/* )
+    config.autoload_paths += %W(#{config.root}/app/models/ckeditor)
   end
 end
